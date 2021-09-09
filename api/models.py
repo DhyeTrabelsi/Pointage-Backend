30eceee3c91d96960ed8846851b8accefcdddbc2
DhyeTrabelsi <mohameddhia.trabelsigerani@enetcom.u-sfax.tn>
2021-09-09 22:41:15 +0100

    full project

---
+++ api/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+from Auth.models import User
+# Create your models here.
+
+class pointage(models.Model):
+    entre   = models.CharField(max_length=30,default="None")
+    sortie  = models.CharField(max_length=30,default="None")
+    date    = models.CharField(max_length=30,default="None")
+    retard  = models.CharField(max_length=30,default="None")
+    absance = models.CharField(max_length=30,default="None")   
+    user    = models.ForeignKey(User, on_delete=models.CASCADE)
+
+
+class salaire(models.Model):
+    mois      = models.IntegerField()
+    heurs_base= models.FloatField(default=0)
+    heurs_sup = models.FloatField(default=0)
+    primes    = models.FloatField(default=0)  
+    total     = models.FloatField(default=0)   
+    user    = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class mission(models.Model):
+    description= models.CharField(max_length=30,default="None")
+    date_debut = models.DateField(default="None")
+    date_fin   = models.DateField(default="None")
+    lieu       = models.CharField(max_length=30,default="None")   
+    etat       = models.CharField(max_length=30,default="None")   
+    user       = models.ForeignKey(User, on_delete=models.SET_NULL , null=True)
+
---
+++ api/views.py
@@ -0,0 +1,83 @@
+from Auth.models import *
+from Auth.serializers import *
+from api.models import *
+from api.serializers import *
+
+from rest_framework.generics import (ListCreateAPIView,RetrieveUpdateAPIView,ListAPIView)
+
+###############################################################################
+
+class SalairesDefineAll(ListCreateAPIView):
+        queryset =salaire.objects.all()
+        serializer_class =salaireSerializer
+        pagination_class = None
+        salaire.objects
+
+class SalaireRetrieve(ListAPIView):
+    serializer_class =salaireSerializer
+    def get_queryset(self):
+        return salaire.objects.filter(user=self.kwargs.get('pk', None)) 
+
+###############################################################################
+
+class PointageDefineAll(ListCreateAPIView):
+        queryset =pointage.objects.all()
+        serializer_class =pointageSerializer
+        pagination_class = None
+        pointage.objects
+
+class PointageRetrieve(ListAPIView):
+    serializer_class =pointageSerializer
+    def get_queryset(self):
+        return pointage.objects.filter(user=self.kwargs.get('pk', None)) 
+
+###############################################################################
+
+class MissionDefineAll(ListCreateAPIView):
+        queryset =mission.objects.all()
+        serializer_class =missionSerializer
+        pagination_class = None
+        mission.objects
+
+class MissionRetrieve(ListAPIView):
+    serializer_class =missionSerializer
+    def get_queryset(self):
+        user = self.request.user
+        return mission.objects.filter(user=self.kwargs.get('pk', None)) 
+
+class MissionRetrieve(ListAPIView):
+    serializer_class =missionSerializer
+    def get_queryset(self):
+        user = self.request.user
+        return mission.objects.filter(user=self.kwargs.get('pk', None)) 
+
+class MissionUpdate(RetrieveUpdateAPIView):
+    serializer_class =missionSerializer
+    def get_queryset(self):
+        user = self.request.user
+        return mission.objects.filter(user=self.kwargs.get('pk', None))# and date=self.kwargs.get('pk2', None)) 
+        #################################
+
+class MissionStarted(RetrieveUpdateAPIView):
+    serializer_class =missionSerializer
+    def get_queryset(self):
+        return mission.objects.filter(id=self.kwargs.get('pk', None))
+
+
+class MissioFinished(RetrieveUpdateAPIView):
+    serializer_class =missionSerializer
+    def get_queryset(self):
+        return mission.objects.filter(id=self.kwargs.get('pk', None))
+
+
+class MissioCanceled(RetrieveUpdateAPIView):
+    serializer_class =missionSerializer
+    def get_queryset(self):
+        return mission.objects.filter(id=self.kwargs.get('pk', None)) 
+
+class MissioWaited(RetrieveUpdateAPIView):
+    serializer_class =missionSerializer
+    def get_queryset(self):
+        return mission.objects.filter(id=self.kwargs.get('pk', None)) 
+
+###############################################################################
